a33dda26d2b86bc79f41a7465df5dad28773ed11
kurpau <paul.kurlavicius@gmail.com>
2023-08-28 10:02:08 +0300

    Add Wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+-- https://wezfurlong.org/wezterm/config/lua/general.html
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.adjust_window_size_when_changing_font_size = false
+config.font = wezterm.font 'Hack Nerd Font Mono'
+config.color_scheme = 'Tokyo Night (Gogh)'
+config.automatically_reload_config = true
+config.audible_bell = 'Disabled'
+config.font_size = 12
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.exit_behavior = "CloseOnCleanExit"
+
+-- and finally, return the configuration to wezterm
+return config
+
