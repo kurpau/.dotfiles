bcf2f7795ab89564556dfe5379b7f90f2f2d9963
kurpau <paul.kurlavicius@gmail.com>
2023-12-14 15:02:58 +0200

    Update plugins

--- nvim/.config/nvim/lua/kurpau/lazy.lua
+++ nvim/.config/nvim/lua/kurpau/lazy.lua
@@ -110,18 +110,7 @@ local plugins = {
 			},
 		},
 	},
-	{
-		-- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help indent_blankline.txt`
-		opts = {
-			char = "┊",
-			show_trailing_blankline_indent = false,
-		},
-	},
-
-	-- "gc" to comment visual regions/lines
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 	{ "numToStr/Comment.nvim", opts = {} },
 
 	{
