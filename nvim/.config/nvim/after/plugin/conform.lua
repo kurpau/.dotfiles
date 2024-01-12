f9288e0ef48ac2be1c918b4c521b904b7d7467d3
kurpau <paul.kurlavicius@gmail.com>
2024-01-12 13:52:39 +0200

    Update formatting config

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -1,20 +1,19 @@
 require("conform").setup({
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { { "prettierd", "prettier" } },
-		typescript = { { "prettierd", "prettier" } },
-		css = { { "prettierd", "prettier" } },
-		html = { { "prettierd", "prettier" } },
-		json = { { "prettierd", "prettier" } },
-		markdown = { { "prettierd", "prettier" } },
-		vue = { { "prettierd", "prettier" } },
-	},
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "isort", "black" },
+    -- Use a sub-list to run only the first available formatter
+    javascript = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
+    json = { { "prettierd", "prettier" } },
+    markdown = { { "prettierd", "prettier" } },
+    vue = { { "prettierd", "prettier" } },
+  },
 })
