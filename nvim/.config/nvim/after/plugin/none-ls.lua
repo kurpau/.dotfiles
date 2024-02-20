8733e75078a76b289f058ba2ddec320b752a37c4
kurpau <paul.kurlavicius@gmail.com>
2024-02-20 11:40:37 +0200

    Add optional eslint diagnostics

--- nvim/.config/nvim/after/plugin/none-ls.lua
+++ nvim/.config/nvim/after/plugin/none-ls.lua
@@ -9,8 +9,12 @@ null_ls.setup({
 		null_ls.builtins.diagnostics.pylint,
 
 		null_ls.builtins.code_actions.eslint_d,
-		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.formatting.eslint_d,
+		null_ls.builtins.diagnostics.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+			end,
+		}),
 	},
 
 	on_attach = function(client, bufnr)
