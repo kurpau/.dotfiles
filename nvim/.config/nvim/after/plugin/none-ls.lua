ba44e0931eec17b15376f403046b318a6fb302b4
kurpau <paul.kurlavicius@gmail.com>
2024-02-20 12:27:02 +0200

    Configure python lsps

--- nvim/.config/nvim/after/plugin/none-ls.lua
+++ nvim/.config/nvim/after/plugin/none-ls.lua
@@ -5,10 +5,11 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.diagnostics.pylint,
+		null_ls.builtins.formatting.ruff_format,
+		null_ls.builtins.diagnostics.ruff,
 
-		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.refactoring,
+
 		null_ls.builtins.formatting.eslint_d,
 		null_ls.builtins.diagnostics.eslint_d.with({
 			condition = function(utils)
