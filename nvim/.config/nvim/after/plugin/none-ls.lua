52c1314b0be6c6642b6734a027bde39871494f2d
kurpau <paul.kurlavicius@gmail.com>
2024-03-07 15:38:35 +0200

    Clean up mason installs

--- nvim/.config/nvim/after/plugin/none-ls.lua
+++ nvim/.config/nvim/after/plugin/none-ls.lua
@@ -2,34 +2,32 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.stylua,
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.black,
 
-		null_ls.builtins.formatting.ruff_format,
-		null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.code_actions.refactoring,
 
-		null_ls.builtins.code_actions.refactoring,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+      end,
+    }),
+  },
 
-		null_ls.builtins.formatting.eslint_d,
-		null_ls.builtins.diagnostics.eslint_d.with({
-			condition = function(utils)
-				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
-			end,
-		}),
-	},
-
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
-	end,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
+  end,
 })
 
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
