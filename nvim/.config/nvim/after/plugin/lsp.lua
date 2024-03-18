860b10cee98fc42338754dd2d5c9465e4582df7c
kurpau <paul.kurlavicius@gmail.com>
2024-03-18 15:05:33 +0200

    Automatic lsp setup

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,29 +1,41 @@
 require("mason").setup()
 
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers({
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup({})
+  end,
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for the `rust_analyzer`:
+  ["lua_ls"] = function()
+    require("lspconfig").lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+  end,
+
+  ["tsserver"] = function()
+    require("lspconfig").tsserver.setup({
+      init_options = {
+        preferences = {
+          disableSuggestions = true,
+        },
+      },
+    })
+  end,
+})
+
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tsserver", "volar", "pyright", "tsserver", "eslint" },
-})
-
-local lspconfig = require("lspconfig")
-
-lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
-})
-lspconfig.tsserver.setup({})
-lspconfig.volar.setup({})
-lspconfig.pyright.setup({})
-lspconfig.tsserver.setup({
-	init_options = {
-		preferences = {
-			disableSuggestions = true,
-		},
-	},
+  ensure_installed = { "lua_ls", "tsserver", "volar", "pyright", "tsserver", "eslint" },
 })
 
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
@@ -31,24 +43,24 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
-	end,
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
 })
 
 vim.diagnostic.config({
-	virtual_text = false,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = false,
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
 })
