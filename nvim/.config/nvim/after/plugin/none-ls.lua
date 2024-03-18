3fa79c68127ff0fb0fb27c6715b86e99ad4eadf6
kurpau <paul.kurlavicius@gmail.com>
2024-03-18 17:23:09 +0200

    Update formatters

--- nvim/.config/nvim/after/plugin/none-ls.lua
+++ nvim/.config/nvim/after/plugin/none-ls.lua
@@ -5,10 +5,12 @@ null_ls.setup({
 	debug = true,
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.black,
 
 		null_ls.builtins.code_actions.refactoring,
+
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.rustywind,
 	},
 
 	on_attach = function(client, bufnr)
