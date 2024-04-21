274bec33232e2ee1dd2ccf04fc61c04b510adee5
kurpau <paul.kurlavicius@gamil.com>
2024-04-21 20:58:32 +0300

    Refactor config

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -5,8 +5,8 @@ vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Quicklist navigation
-vim.keymap.set("n", "<M-j>", ":cnext<cr>")
-vim.keymap.set("n", "<M-k>", ":cprev<cr>")
+vim.keymap.set("n", "<C-S-k>", ":cprev<cr>")
+vim.keymap.set("n", "<C-S-j>", ":cnext<cr>")
 
 -- ============== toggle file explorer (nvim-tree) ============
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<cr>")
@@ -39,3 +39,23 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- edit all the same words you're currently on
 vim.keymap.set("n", "<leader>ew", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -48,3 +48,12 @@ vim.g.netrw_banner = 0
 vim.g.netrw_preview = 1
 vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 30
+
+-- diagnostics
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = false,
+})
