7dd3f54df81177caa0cd0f991dec3cc8f66e4d77
kurpau <paul.kurlavicius@gamil.com>
2024-06-05 00:22:25 +0300

    Change colorscheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,14 +8,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 config.hide_mouse_cursor_when_typing = false
 config.adjust_window_size_when_changing_font_size = false
 config.font = wezterm.font("Hack Nerd Font Mono")
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.automatically_reload_config = true
 config.audible_bell = "Disabled"
 config.font_size = 16
@@ -23,12 +23,12 @@ config.window_decorations = "TITLE | RESIZE"
 config.enable_tab_bar = false
 config.exit_behavior = "CloseOnCleanExit"
 config.cursor_blink_rate = 0
-config.window_background_opacity = 0.95
+-- config.window_background_opacity = 0.95
 config.initial_rows = 30
 config.initial_cols = 80
 config.line_height = 1.05
 config.colors = {
-  cursor_fg = "black",
+	cursor_fg = "black",
 }
 
 -- and finally, return the configuration to wezterm
