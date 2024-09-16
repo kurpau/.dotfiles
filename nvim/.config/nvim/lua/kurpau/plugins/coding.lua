96ec41235a6c5d93876fb480e02eb8d80d5a6e10
Paulius Kurlavičius <paul.kurlavicius@gmail.com>
2024-09-16 10:47:03 +0300

    Remove avante

--- nvim/.config/nvim/lua/kurpau/plugins/coding.lua
+++ nvim/.config/nvim/lua/kurpau/plugins/coding.lua
@@ -255,27 +255,4 @@ return {
 		"jsongerber/nvim-px-to-rem",
 		config = true,
 	},
-
-	-- LLM
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- add any opts here
-		},
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below is optional, make sure to setup it properly if you have lazy=true
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
-	},
 }
