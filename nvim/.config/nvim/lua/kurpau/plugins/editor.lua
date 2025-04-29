315d8ff2af14b093a7dc28803604f04050692787
Paulius Kurlavičius <paul.kurlavicius@gmail.com>
2025-04-29 09:04:36 +0300

    Add telescope extensions

--- nvim/.config/nvim/lua/kurpau/plugins/editor.lua
+++ nvim/.config/nvim/lua/kurpau/plugins/editor.lua
@@ -42,11 +42,39 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-telescope/telescope-live-grep-args.nvim",
+			"nvim-lua/plenary.nvim",
+		},
 		config = function()
+			local telescope = require("telescope")
+			local lga_actions = require("telescope-live-grep-args.actions")
 			local builtin = require("telescope.builtin")
+
 			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+			-- vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+			vim.keymap.set(
+				"n",
+				"<leader>ps",
+				":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
+			)
+			vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
+
+			telescope.setup({
+				extensions = {
+					live_grep_args = {
+						auto_quoting = true,
+						mappings = {
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+								["<C-space>"] = lga_actions.to_fuzzy_refine,
+							},
+						},
+					},
+				},
+			})
+			telescope.load_extension("live_grep_args")
 		end,
 	},
 
